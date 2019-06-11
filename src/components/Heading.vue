eafe83cd4c56b2facf04092f8d523ffadc42425a
Reshma Krishnan <Reshma.Krishnan@intusurg.com>
2019-06-11 18:03:46 +0530

    added files

---
+++ src/components/Heading.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1 :style="headingStyles">{{title}}</h1>
+  <h3>hi</h3>
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headingStyles: {
+        color: this.color
+      }
+    };
+  },
+  props: ["title", "color"]
+};
+</script>
